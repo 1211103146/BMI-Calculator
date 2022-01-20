75ba7ee420975e0df1fb83aac1a49ba1576b864f
M-night3246 <98091657+M-night3246@users.noreply.github.com>
2022-01-20 11:28:11 +0000

    Update BMI.cpp
    Added loop for invalid user inputs. Added BMI classifications and associated health risks.

--- BMI.cpp
+++ BMI.cpp
@@ -1,33 +1,57 @@
 #include <iostream>
 using namespace std;
 
-main() {
+main()
+{
 
 float height, weight, BMI;
 
 cout<<"BMI CALCULATOR"<<endl;
 cout<<"What is your height in meters?"<<endl;
 
-cin>>height;
+while (true)
+{
+    cin>>height;
 
-if (height<0.5) {
-    cout<<"Children don't need to measure their BMI!"<<endl;
-    cout<<"Please input your height in meters again."<<endl;
-    cin>>height; }
+    if (height<0.5) {
+        cout<<"Children don't need to measure their BMI!"<<endl;
+        cout<<"Please input your height in meters again."<<endl; }
 
-else if (height>=3.0) {
-    cout<<"Yer a giant, Harry!"<<endl;
-    cout<<"Please input your height in meters again."<<endl;
-    cin>>height; }
+    else if (height>=3.0) {
+        cout<<"Yer a giant, Harry!"<<endl;
+        cout<<"Please input your height in meters again."<<endl; }
 
-else if (height>=2.0) {
+    else
+        break;
+}
+
+if (height>=2.0) {
     cout<<"You're tall!"<<endl; }
 
-
 cout<<"What is your weight?"<<endl;
-cin>>weight;
+    cin>>weight;
 
 BMI=weight/(height*height);
 
 cout<<"Your BMI is: "<<BMI<<endl;
+
+if (BMI<18.5) {
+    cout<<"Underweight"<<endl;
+    cout<<"Associated health risks: Moderate"<<endl; }
+    
+if (BMI>=18.5 && BMI<=24.9) {
+    cout<<"Normal"<<endl;
+    cout<<"Associated health risks: Low"<<endl; }
+
+if (BMI>=25 && BMI<=29.9) {
+    cout<<"Overweight"<<endl;
+    cout<<"Associated health risks: Moderate"<<endl; }
+
+if (BMI>=30 && BMI<=39.9) {
+    cout<<"Obese"<<endl;
+    cout<<"Associated health risks: High"<<endl; }
+
+if (BMI>=40) {
+    cout<<"Morbidly Obese"<<endl;
+    cout<<"Associated health risks: Severe"<<endl; }
 }
